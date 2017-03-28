629f12dd0d65a87595e3945910d42e0a1d361e0e
claudinei18 <claudineigom18@gmail.com>
2017-03-28 08:48:29 -0300

    initial commit

---
+++ src/main/java/com/theseeker/TheSeekerApplication.java
@@ -0,0 +1,32 @@
+package com.theseeker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class TheSeekerApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(TheSeekerApplication.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication app = new SpringApplication(TheSeekerApplication.class);
+		SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+		ConfigurableApplicationContext run = app.run(args);
+		run.registerShutdownHook();
+		Environment env = run.getEnvironment();
+		log.info("Access URLs:\n----------------------------------------------------------\n\t" +
+						"Local: \t\thttp://127.0.0.1:{}\n\t" +
+						"External: \thttp://{}:{}\n----------------------------------------------------------",
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
+	}
+}
